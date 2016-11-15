b6271b5b69a06a0a8b861cd0a8dc312b0e2493d5
Tim Hanson <TimTHanson@bahn.de>
2016-11-15 14:39:51 +0000

    still in step 3 with onClick -- still runs

--- app/app.component.ts
+++ app/app.component.ts
@@ -27,14 +27,23 @@ const HEROES: Hero[] = [
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes">
-        <script>
-      console.log(" - test logging inside loop: " + hero.name);
-</script>
+    <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
+
   </ul>
+
+  <div *ngIf="selectedHero">
+    <h2>{{selectedHero.name}} details!</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+    </div>
+  </div>
+
   `,
+
 
   styles: [`
   .selected {
@@ -90,10 +99,11 @@ const HEROES: Hero[] = [
 
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   heroes = HEROES;
 }
