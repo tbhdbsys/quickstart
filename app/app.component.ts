12962abe4ed29b7a53f0409f8eaa80a56de8b54c
Hanson <TimTHanson@bahn.de>
2016-11-15 09:20:25 +0000

    2nd baby step -- still runs

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+
 @Component({
     selector: 'my-app',
 //    template: `<h1>Hello Angular</h1>`
-    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
