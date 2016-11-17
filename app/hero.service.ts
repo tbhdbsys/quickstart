d25a313ccc10ab5ec5c02f8d6fc1f06f79ab8018
Tim Hanson <TimTHanson@bahn.de>
2016-11-17 13:36:25 +0000

    7) http: first step

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,10 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+
+
+
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'app/heroes';  // URL to web api
+
+  constructor(private http: Http) { }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
@@ -13,10 +31,9 @@ export class HeroService {
       .then(() => this.getHeroes());
   }
 
-  getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-      .then(heroes => heroes.find(hero => hero.id === id));
-  }
+
+
+
 
 }
 
