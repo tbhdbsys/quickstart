942d32acda78f3434750aa6d0f299061380a22a1
Tim Hanson <TimTHanson@bahn.de>
2016-11-17 11:32:13 +0000

    finished part 6

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,15 +6,15 @@ import {Component} from "@angular/core";
   selector: 'my-app',
 
   template: `
-   <h1>{{title}}</h1>
-   <nav>
-     <a routerLink="/dashboard">Dashboard</a>
-     <a routerLink="/heroes">Heroes</a>
-   </nav>
-   <router-outlet></router-outlet>
- `,
+  <h1>{{title}}</h1>
+  <nav>
+    <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+    <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+  </nav>
+  <router-outlet></router-outlet>
+`,
 
-  styleUrls: [ 'dashboard.component.css' ]
+  styleUrls: ['app.component.css']
 })
 
 export class AppComponent {
